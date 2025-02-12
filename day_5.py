c7d908536d8ec1dcea464572bcbe762f27fab246
Conor Durkin <conordurkin@me.com>
2025-02-11 20:05:04 -0600

    part 2 done - suprising how fast this ran tbh. tried a 'move left' function at first and it was excruciatingly slow

--- day_5.py
+++ day_5.py
@@ -39,3 +39,38 @@ for i in range(len(checked)):
         score += updates_parsed[i][use]
 
 print("Part 1: " + str(score))
+
+# Okay, onto Part 2: 
+
+# Keep only the incorrect ones 
+incorrect = []
+for i in range(len(checked)):
+    if checked[i] == False:
+        incorrect.append(updates_parsed[i])
+
+# Function to swap entries if needed
+def swap_entries(lst, i, j):
+    lst[i], lst[j] = lst[j], lst[i]
+
+# Try every rule, swap any times they're wrong. Go til they're right.
+for entry in incorrect:
+    status = False
+    while status == False:
+        status = True
+        for rule in rules_parsed: 
+            if rule[0] in entry:
+                if rule[1] in entry:
+                    if entry.index(rule[0]) > entry.index(rule[1]):
+                        a = entry.index(rule[1])
+                        b = entry.index(rule[0])
+                        swap_entries(entry, a, b) 
+                        status = False
+        if status == True:
+            continue 
+
+score = 0 
+for i in range(len(incorrect)):
+    use = math.floor(len(incorrect[i])/2)
+    score += incorrect[i][use]
+
+print("Part 2: " + str(score))
